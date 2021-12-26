7b1d3884a2b7b4f638db37ac8779904d3fc31159
Sora-210 <sokominto1231@gmail.com>
2021-12-26 19:10:19 +0000

    [update] Logs系-認証込み

--- back/src/Router/Logs.ts
+++ back/src/Router/Logs.ts
@@ -1,7 +1,7 @@
 import { DB } from '../db'
 import { MessageResponse } from '../interface/MessageResponse'
 import { Router } from 'express'
-import { FindOptions } from 'sequelize'
+import { tokenAuth } from '../function/Auth'
 
 const LogsRouter:Router = Router()
 
@@ -20,7 +20,10 @@ LogsRouter.get('/', async(req, res) => {
     const offset = limit * (pages - 1)
     let options:any = {
         limit: limit,
-        offset: offset
+        offset: offset,
+        order: [
+            ['createdAt','DESC']
+        ]
     }
     if (req.query.userId) {
         options.where.userId = req.query.userId
@@ -51,55 +54,100 @@ LogsRouter.get('/:logId', async(req, res) => {
     }
 })
 
+LogsRouter.use(tokenAuth)
 LogsRouter.post('/', async(req, res) => {
     const date = new Date()
-    if (req.body.title != null && req.body.targetId != null) {
-        const Data = {
-            targetId: req.body.targetId,
-            title: req.body.title,
-            detail: req.body.detail,
-            date: date
-        }
-
-        const Log = await DB.Logs.create(Data)
-
-        res.status(200).json(Log)
-    } else {
+    if (req.body.title == null || req.body.targetId == null) {
         const resMes: MessageResponse = {
             status: "Error",
             message: "Not Enogth Property"
         }
         res.status(403).json(resMes)
+        return 0
     }
+    const isTargetId = await DB.Targets.findOne({
+        where: {
+            targetId: req.body.targetId
+        }
+    })
+    if(!isTargetId) {
+        const resMes: MessageResponse = {
+            status: "Error",
+            message: "Not Found"
+        }
+        res.status(404).json(resMes)
+        return 0
+    }
+    if(isTargetId.userId === req.uid) {
+        const resMes: MessageResponse = {
+            status: "Error",
+            message: "Not Enogth Authority"
+        }
+        res.status(403).json(resMes)
+        return 0
+    }
+    const Data = {
+        targetId: req.body.targetId,
+        title: req.body.title,
+        detail: req.body.detail,
+        date: date
+    }
+    const Log = await DB.Logs.create(Data)
+    res.status(200).json(Log)
 })
 LogsRouter.delete('/:logId', async(req, res) => {
-    if (req.params.logId != null) {
-        const options = {
-            where: {
-                id: req.params.logId
-            }
-        }
-        const dbRes = await DB.Logs.destroy(options)
-
-        if (dbRes) {
-            const resMes: MessageResponse = {
-                status: "success",
-                message: "Deleted"
-            }
-            res.status(200).json(resMes)
-        } else {
-            const resMes: MessageResponse = {
-                status: "Error",
-                message: "Not Found"
-            }
-            res.status(404).json(resMes)
-        }
-    } else {
+    if (req.params.logId == null) {
         const resMes: MessageResponse = {
             status: "Error",
             message: "Not Enogth Property"
         }
+        res.status(402).json(resMes)
+        return 0
+    }
+    const isLog = await DB.Logs.findOne({
+        where: {
+            id: req.params.logId
+        }
+    })
+    if(!isLog) {
+        const resMes: MessageResponse = {
+            status: "Error",
+            message: "Not Found"
+        }
+        res.status(404).json(resMes)
+        return 0
+    }
+    const isTarget = await DB.Targets.findOne({
+        where: {
+            id: isLog.targetId,
+            userId: req.uid
+        }
+    })
+    if(!isTarget) {
+        const resMes: MessageResponse = {
+            status: "Error",
+            message: "Not Enogth Authority"
+        }
         res.status(403).json(resMes)
+        return 0
+    }
+    const dbRes = await DB.Logs.destroy({
+        where: {
+            id: req.params.logId
+        }
+    })
+    if (dbRes) {
+        const resMes: MessageResponse = {
+            status: "success",
+            message: "Deleted"
+        }
+        res.status(200).json(resMes)
+    } else {
+        const resMes: MessageResponse = {
+            status: "Error",
+            message: "Unknown Error"
+        }
+        res.status(500).json(resMes)
     }
 })
 //=====================================
