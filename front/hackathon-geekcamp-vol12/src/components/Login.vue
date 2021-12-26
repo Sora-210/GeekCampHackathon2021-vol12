4f665fb9cb2db481a746b2ea373833b0fec030e4
mochi-yu <mochi.2225.it@gmail.com>
2021-12-27 06:51:02 +0900

    [add] ログインページを追加

---
+++ front/hackathon-geekcamp-vol12/src/components/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <div :class="[`text-h3`,`mb-8`]" class="transition-swing" v-text="'ログイン'"></div>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
