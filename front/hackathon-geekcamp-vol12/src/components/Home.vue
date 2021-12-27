f2c62168a42b1907fd7843fd536c9b02666ac6ae
mochi-yu <mochi.2225.it@gmail.com>
2021-12-27 14:42:25 +0900

    [fix] 機能の修正

--- front/hackathon-geekcamp-vol12/src/components/Home.vue
+++ front/hackathon-geekcamp-vol12/src/components/Home.vue
@@ -7,8 +7,6 @@
           <v-list two-line>
             <v-card v-for="data in postData" :key="data.detail" class="my-8" elevation="2" outlined shaped>
               <v-list-item>
-                <v-list-item-avatar color="grey darken-1"></v-list-item-avatar>
-
                 <v-list-item-content>
                   <v-list-item-title>{{ data.title }}</v-list-item-title>
                   <v-list-item-subtitle>UserID: {{ data.id }}</v-list-item-subtitle>
@@ -31,51 +29,6 @@ const URL = 'https://dev.mylog.sora210.dev/api'
 export default {
   data: () => ({
     postData: '',
-    sampleData: [{
-      'id': 0,
-      'targetId': 0,
-      'title': 'User1',
-      'detail': 'test01',
-      'date': '2021-12-26',
-      'createdAt': '2021-12-26',
-      'updatedAt': '2021-12-26'
-    },
-    {
-      'id': 0,
-      'targetId': 0,
-      'title': 'User1',
-      'detail': 'test03',
-      'date': '2021-12-25',
-      'createdAt': '2021-12-25',
-      'updatedAt': '2021-12-26'
-    },
-    {
-      'id': 0,
-      'targetId': 0,
-      'title': 'User1',
-      'detail': 'test04',
-      'date': '2021-12-24',
-      'createdAt': '2021-12-24',
-      'updatedAt': '2021-12-24'
-    },
-    {
-      'id': 0,
-      'targetId': 0,
-      'title': 'User1',
-      'detail': 'test02',
-      'date': '2021-12-25',
-      'createdAt': '2021-12-25',
-      'updatedAt': '2021-12-26'
-    },
-    {
-      'id': 1,
-      'targetId': 0,
-      'title': 'User2',
-      'detail': 'test05',
-      'date': '2021-12-24',
-      'createdAt': '2021-12-24',
-      'updatedAt': '2021-12-24'
-    }],
     date: ''
   }),
   mounted () {
